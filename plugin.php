3e8e1adf88fab6a8f844e166eb963f76466dcf4c
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:51:53 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_random_number_generator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Random Number Generator by Calculator.iO";
 
 function display_ci_random_number_generator(){
